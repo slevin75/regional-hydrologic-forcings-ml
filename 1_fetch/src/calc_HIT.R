ac396b1c43949d1f6220839438f2eeec1c28ea9e
Smith <jsmith@usgs.gov>
2021-12-21 09:27:30 -0500

    Added argument for statistics to compute. Some minor formating edits.

--- 1_fetch/src/calc_HIT.R
+++ 1_fetch/src/calc_HIT.R
@@ -1,21 +1,23 @@
-
-calc_HITmetrics<-function(site_num,clean_daily_flow,yearType,drainArea_tab,floodThreshold_tab){
+#Add description
+calc_HITmetrics <- function(site_num, clean_daily_flow, yearType, 
+                            drainArea_tab, floodThreshold_tab, stat_vec){
   print(site_num)
-  data<- clean_daily_flow %>%
+  data <- clean_daily_flow %>%
     filter(site_no == site_num)
 
   drainArea <- drainArea_tab %>%
-    filter(site_no == site_num)%>%
+    filter(site_no == site_num) %>%
     pull(drainArea)
  
-  floodThreshold<- floodThreshold_tab %>%
-    filter(site_no == site_num)%>%
+  floodThreshold <- floodThreshold_tab %>%
+    filter(site_no == site_num) %>%
     pull(floodThreshold)
 
-  out_data<- suppressWarnings(calc_allHIT(x=data,yearType=yearType,
+  out_data <- suppressWarnings(calc_allHIT(x=data, yearType=yearType,
                                           drainArea=drainArea,
-                                          floodThreshold=floodThreshold))
+                                          floodThreshold=floodThreshold, 
+                                          stats = stat_vec))
   
-  out_data$site_num<-unique(data$site_no)
+  out_data$site_num <- unique(data$site_no)
   return(out_data)
 }

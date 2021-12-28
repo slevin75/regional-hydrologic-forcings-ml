29c0ad77244768ae8e22b8e5ce1d9acc6099ed94
JaredSmith <jsmith@usgs.gov>
2021-12-28 09:14:35 -0500

    Changed conditional replacements to tidy format

--- 1_fetch/src/calc_HIT.R
+++ 1_fetch/src/calc_HIT.R
@@ -31,15 +31,17 @@ calc_HITmetrics <- function(site_num, clean_daily_flow, yearType,
   
   #Normalize metrics by drainage area
   if (!is.null(norm_DA)){
-    out_data$statistic[out_data$indice %in% norm_DA] <- round(
-      out_data$statistic[out_data$indice %in% norm_DA]/drainArea, digits)
+    out_data <- out_data %>% mutate(statistic = case_when(
+      (indice %in% norm_DA) ~ round(statistic/drainArea, digits),
+      TRUE ~ statistic))
   }
   
   #Normalize metrics by drainage area and remove median normalization
   if (!is.null(norm_med_DA)){
     med_flow <- median(data$discharge, na.rm = TRUE)
-    out_data$statistic[out_data$indice %in% norm_med_DA] <- round(
-      out_data$statistic[out_data$indice %in% norm_med_DA]/drainArea*med_flow, digits)
+    out_data <- out_data %>% mutate(statistic = case_when(
+      (indice %in% norm_med_DA) ~ round(statistic/drainArea*med_flow, digits),
+      TRUE ~ statistic))
   }
   
   #Normalize ml17 by drainage area and remove annual mean normalization
@@ -47,13 +49,15 @@ calc_HITmetrics <- function(site_num, clean_daily_flow, yearType,
     calc_bfibyyear <- dplyr::summarize(dplyr::group_by(data, year_val), 
                                        calc_bfi = calc_bfi(discharge)*mean(discharge))
     
-    out_data$statistic[out_data$indice %in% 'ml17'] <- round(
-      mean(calc_bfibyyear$calc_bfi)/drainArea, digits)
+    out_data <- out_data %>% mutate(statistic = case_when(
+      (indice %in% 'ml17') ~ round(mean(calc_bfibyyear$calc_bfi)/drainArea, digits),
+      TRUE ~ statistic))
     
     #Correct the CV of ml18 if it's one of the saved metrics
     if(!is.null(save_metrics) & ('ml18' %in% save_metrics)){
-      out_data$statistic[out_data$indice %in% 'ml18'] <- round(
-        sd(calc_bfibyyear$calc_bfi)/mean(calc_bfibyyear$calc_bfi) * 100, digits)
+      out_data <- out_data %>% mutate(statistic = case_when(
+        (indice %in% 'ml18') ~ round(sd(calc_bfibyyear$calc_bfi)/mean(calc_bfibyyear$calc_bfi) * 100, digits),
+        TRUE ~ statistic))
     }
   }
   return(out_data)

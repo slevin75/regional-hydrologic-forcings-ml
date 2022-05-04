95525809d4833882454a6fa416500aaac0e7f639
Jared D. Smith <jsmith@usgs.gov>
2022-05-04 13:03:47 -0500

    Updated refine features to accept object instead of file path. Updated correlation check to be general to whatever variable prefix is supplied

--- 5_EDA/src/select_features.R
+++ 5_EDA/src/select_features.R
@@ -1,16 +1,14 @@
-refine_features <- function(nhdv2_attr_path, drop_columns){
+refine_features <- function(nhdv2_attr, drop_columns){
   #' 
   #' @description Function to reduce and refine the features for use in models.
   #' It drops features that have the same value for all gages.
   #' It drops columns specified in drop_columns
   #' It converts -9999 to NA
   #'
-  #' @param nhdv2_attr_path the csv of static attributes (columns) for each COMID (rows)
+  #' @param nhdv2_attr the tbl of static attributes (columns) for each COMID (rows)
   #' @param drop_columns character vector of column names to remove from nhdv2_attr
   #' 
   #' @value Returns a refined nhdv2_attr based on the columns to drop
-  
-  nhdv2_attr <- read_csv(nhdv2_attr_path, show_col_types = FALSE)
   
   #Convert -9999 to NA
   nhdv2_attr[nhdv2_attr == -9999] <- NA
@@ -24,16 +22,17 @@ refine_features <- function(nhdv2_attr_path, drop_columns){
   return(nhdv2_attr_refined)
 }
 
-drop_high_corr_ACC <- function(features, threshold_corr){
+drop_high_corr_ACCTOT <- function(features, threshold_corr, drop_var){
   #' 
-  #' @description Function to remove ACC attributes that are highly correlated
+  #' @description Function to remove ACC or TOT attributes that are highly correlated
   #' with other attributes.
   #'
   #' @param features table of static attributes (columns) for each gage. Must have
   #' columns for COMID and GAGES_ID
   #' @param threshold_corr correlation threshold used to drop ACC attributes
+  #' @param drop_var the variable prefix to drop. "ACC" or "TOT"
   #' 
-  #' @value Returns features without highly correlated ACC attributes
+  #' @value Returns features without highly correlated drop_var attributes
   
   #Correlation matrix
   cor_mat <- cor(features %>% select(-COMID, -GAGES_ID))
@@ -44,32 +43,31 @@ drop_high_corr_ACC <- function(features, threshold_corr){
   high_corr_features <- high_corr_features[
     -which(high_corr_features[,1] == high_corr_features[,2]),]
   
-  #Find where ACC is highly correlated with CAT, TOT, or 
-  #"npdes" "fwwd"  "strg"  "devl"  "cndp" and remove
+  #Find where drop_var is highly correlated with other variables and remove
   names_cor <- colnames(cor_mat)
-  remove_ACC <- vector('character', length = 0L)
+  remove_vars <- vector('character', length = 0L)
   for(i in 1:nrow(high_corr_features)){
     name <- rownames(high_corr_features)[i]
-    if(substr(name, 1,3) == 'ACC'){
-      #Check if the column is also ACC
-      if(substr(names_cor[high_corr_features[i,2]], 1,3) != 'ACC'){
-        #add the ACC name to vector for removal
-        remove_ACC <- c(remove_ACC, name)
+    if(substr(name, 1,3) == drop_var){
+      #Check that the column is not a drop_var prefix
+      if(substr(names_cor[high_corr_features[i,2]], 1,3) != drop_var){
+        #add the drop_var name to vector for removal
+        remove_vars <- c(remove_vars, name)
       }
     }
   }
-  remove_ACC <- unique(remove_ACC)
+  remove_vars <- unique(remove_vars)
   
-  #Drop all of the remove_ACC values - we'll use TOT instead
-  features <- select(features, -{{remove_ACC}})
+  #Drop all of the remove_vars
+  features <- select(features, -{{remove_vars}})
   
-  #other correlations were checked manually for CAT and TOT using
-  #cor_mat <- cor(features %>% select(starts_with('TOT_')))
+  #other correlations were checked manually using
+  #cor_mat <- cor(features %>% select(starts_with('CAT_')))
   #high_corr_features <- which(cor_mat >= 0.9, arr.ind = T)
   #high_corr_features <- rownames(high_corr_features[
   #  -which(high_corr_features[,1] == high_corr_features[,2]),])
   
-  #Then the interaction of CAT and TOT for non-climate variables:
+  #Then the interaction of CAT and ACC/TOT for non-climate variables:
   #cor_mat <- cor(features[,-c(1,2)] %>% select(-contains('_PPT_'), 
   #-contains('_TAV_'), -contains('WB5100')))
   

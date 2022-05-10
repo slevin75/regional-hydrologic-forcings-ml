e8196bbc5965432ff8cb14fbb6b1819342947fe4
Jared D. Smith <jsmith@usgs.gov>
2022-05-10 08:47:24 -0500

    added functions that allow using exact test data from other regions

--- 6_predict/src/train_models.R
+++ 6_predict/src/train_models.R
@@ -60,7 +60,7 @@ screen_Boruta <- function(features, cluster_table, metrics_table, metric_name,
   }
   
   #Select training region
-  if(train_region == 'all'){
+  if('all' %in% train_region){
     metrics_table$region = 'all'
   }else{
     #select only gages for high and mid-high model regions
@@ -97,6 +97,194 @@ screen_Boruta <- function(features, cluster_table, metrics_table, metric_name,
   
   #Split into training and testing datasets
   input_data_split <- split_data(input_data, train_prop = train_prop)
+  
+  #Apply Boruta to down-select features
+  #This is parallelized by default
+  #Noticed that there were switches when applied 2 times, probably due to correlation
+  #applying once to CAT+dev only, then ACC+dev only
+  brf_noACC <- Boruta(x = input_data_split$training %>% 
+                        select(-COMID, -GAGES_ID, -{{metric_name}}, -starts_with('ACC_')) %>%
+                        as.data.frame(),
+                      y = input_data_split$training %>% 
+                        pull({{metric_name}}),
+                      pValue = 0.01,
+                      mcAdj = TRUE,
+                      maxRuns = brf_runs,
+                      doTrace = 0,
+                      holdHistory = TRUE,
+                      getImp = getImpRfZ,
+                      num.trees = ntrees,
+                      oob.error = TRUE,
+                      num.threads = ncores)
+  
+  brf_noCAT <- Boruta(x = input_data_split$training %>% 
+                        select(-COMID, -GAGES_ID, -{{metric_name}}, -starts_with('CAT_')) %>%
+                        as.data.frame(),
+                      y = input_data_split$training %>% 
+                        pull({{metric_name}}),
+                      pValue = 0.01,
+                      mcAdj = TRUE,
+                      maxRuns = brf_runs,
+                      doTrace = 0,
+                      holdHistory = TRUE,
+                      getImp = getImpRfZ,
+                      num.trees = ntrees,
+                      oob.error = TRUE,
+                      num.threads = ncores)
+  
+  brf_All <- Boruta(x = input_data_split$training %>%
+                      select(-COMID, -GAGES_ID, -{{metric_name}}) %>%
+                      as.data.frame(),
+                    y = input_data_split$training %>%
+                      pull({{metric_name}}),
+                    pValue = 0.01,
+                    mcAdj = TRUE,
+                    maxRuns = brf_runs,
+                    doTrace = 0,
+                    holdHistory = TRUE,
+                    getImp = getImpRfZ,
+                    num.trees = ntrees,
+                    oob.error = TRUE,
+                    num.threads = ncores)
+  
+  #Select all features that were not rejected over these 3 screenings
+  names_unique = unique(c(names(brf_All$finalDecision[brf_All$finalDecision != 'Rejected']),
+                          names(brf_noACC$finalDecision[brf_noACC$finalDecision != 'Rejected']),
+                          names(brf_noCAT$finalDecision[brf_noCAT$finalDecision != 'Rejected'])
+  ))
+  
+  #Create modeling dataset
+  #Make the class match the split object
+  screened_input_data <- list(split = input_data_split$split,
+                              training = input_data_split$training %>% 
+                                select(COMID, GAGES_ID, all_of(names_unique), 
+                                       {{metric_name}}),
+                              testing = input_data_split$testing %>% 
+                                select(COMID, GAGES_ID, all_of(names_unique), 
+                                       {{metric_name}}))
+  #correcting the split table separately so that the class of the split object
+  #is correct.
+  screened_input_data$split$data <- screened_input_data$split$data %>% 
+    select(COMID, GAGES_ID, all_of(names_unique), {{metric_name}})
+  
+  # metric name, all 3 brf models and the input dataset (IDs, features, metric)
+  return(list(metric = metric_name, brf_noCAT = brf_noCAT, brf_noACC = brf_noACC, 
+              brf_All = brf_All, input_data = screened_input_data))
+}
+
+screen_Boruta_exact <- function(features, cluster_table, metrics_table, metric_name,
+                          train_region, ncores, brf_runs, ntrees, train_prop,
+                          exact_test_data){
+  #' 
+  #' @description Applies Boruta screening to the features. Makes a train/test
+  #' split before applying the screening.
+  #'
+  #' @param features table of gages (rows) and features (columns). Must include
+  #' COMID and GAGES_ID columns.
+  #' @param cluster_table table of gages (rows) and cluster columns. Must include 
+  #' columns for gage ID, and regions named midhigh and high.
+  #' @param metrics_table table of metrics computed for each gage. Must include
+  #' site_num column.
+  #' @param metric_name character string of the column name in metrics_table to use
+  #' @param train_region vector of 'rain', 'snow', or both. Can use 'all' to 
+  #' use all gages in the metrics_table.
+  #' @param ncores number of cores to use
+  #' @param brf_runs maximum number of RF runs
+  #' @param ntrees number of trees to use
+  #' @param train_prop proportion of the data to use for training
+  #' @param exact_test_data data to ensure is in the testing dataset
+  #' 
+  #' @return Returns a list of the metric name, all 3 brf models, and 
+  #' the input dataset (IDs, features, metric) as a list with train/test splits
+  #' as the elements of the list.
+  
+  
+  #Get only the metric_name metric
+  metrics_table <- select(metrics_table, site_num, .data[[metric_name]])
+  
+  #determine if the metric should use the high flow region or mid-high flow region
+  if (metric_name %in% c('ma','ml17', 'ml18')){
+    region <- 'midhigh'
+  } else if (!grepl("_q", metric_name)){
+    ##other metrics from HIT
+    region <- 'high'
+  }else{
+    ##get quantile from FDC metric name
+    metric_quantile <- as.numeric(str_split(metric_name,pattern="_q")[[1]][2])
+    region <- ifelse(metric_quantile < 0.75, 'midhigh', 'high')
+  }
+  
+  #Select training region
+  if('all' %in% train_region){
+    metrics_table$region = 'all'
+  }else{
+    #select only gages for high and mid-high model regions
+    if (region == 'high'){
+      metrics_table <- mutate(metrics_table,
+                              region = case_when(site_num %in% cluster_table$ID[cluster_table$high == 4] ~ 'snow',
+                                                 site_num %in% cluster_table$ID[cluster_table$high == 2] ~ 'rain')) %>%
+        drop_na()
+    }else{
+      metrics_table <- mutate(metrics_table,
+                              region = case_when(site_num %in% cluster_table$ID[cluster_table$midhigh == 5] ~ 'snow',
+                                                 site_num %in% cluster_table$ID[cluster_table$midhigh == 3] ~ 'rain'))  %>%
+        drop_na()
+    }
+  }
+  
+  #Select the features for these gages
+  features <- filter(features, GAGES_ID %in% metrics_table$site_num) %>%
+    left_join(metrics_table %>% select(site_num, region), c('GAGES_ID' = 'site_num')) %>%
+    filter(region %in% train_region)
+  
+  #Detect variables that are all equal across the modeling domain and remove them
+  unique_col_vals <- apply(features, 2, FUN = function(x) length(unique(x[!is.na(x)])))
+  features <- features[, which(unique_col_vals > 1)]
+  
+  #Join dataframe to match the features to the metrics
+  input_data <- left_join(features, metrics_table %>% select(-region), 
+                          by = c('GAGES_ID' = 'site_num'))
+  
+  #Split into training and testing datasets
+  #use the exact_test_data:
+  if (all(c('rain', 'snow') %in% train_region)){
+    #use this to get a random split - ensures the classes are correct
+    input_data_split <- split_data(input_data, train_prop = train_prop)
+    #replace entries with the exact_test_data
+    input_data_split$split$in_id <- which(!(input_data_split$split$data$GAGES_ID %in% 
+                                            exact_test_data))
+    input_data_split$training <- input_data_split$split$data[input_data_split$split$in_id,]
+    input_data_split$testing <- input_data_split$split$data[-input_data_split$split$in_id,]
+  }else{
+    #add cluster information to the table
+    input_data <- left_join(input_data, cluster_table %>% select(ID, {{region}}), 
+                            by = c('GAGES_ID' = 'ID'))
+    
+    if(region == 'high'){
+      #randomize for clusters 1, 3, and 5
+      input_data_split3 <- split_data(input_data %>% filter(.data[[region]] %in% c(1,3,5)), 
+                                        train_prop = train_prop)
+    }else{
+      #randomize for clusters 1, 2, and 4
+      input_data_split3 <- split_data(input_data %>% filter(.data[[region]] %in% c(1,2,4)), 
+                                        train_prop = train_prop)
+    }
+    
+    #add on the test data from exact_test_data
+    #include all other training data from rain and snow regions
+    input_data_split <- input_data_split3
+    input_data_split$split$data <- input_data
+    input_data_split$testing <- rbind(input_data_split3$testing,
+                                      input_data[which(input_data$GAGES_ID %in% exact_test_data),])
+    input_data_split$training <- input_data[which(!(input_data$GAGES_ID %in% input_data_split$testing$GAGES_ID)),]
+    input_data_split$split$in_id <- which(input_data_split$split$data$GAGES_ID %in% 
+                                            input_data_split$training$GAGES_ID)
+    
+    #Remove the cluster column
+    input_data_split$training <- select(input_data_split$training, -{{region}})
+    input_data_split$testing <- select(input_data_split$testing, -{{region}})
+    input_data_split$split$data <- select(input_data_split$split$data, -{{region}})
+  }
   
   #Apply Boruta to down-select features
   #This is parallelized by default
@@ -382,5 +570,80 @@ predict_test_data <- function(model_wf, features, cluster_table, metrics_table,
   
   perf_metrics <- metrics(test_preds, truth = 'obs', estimate = '.pred')
 
+  return(list(metric = metric_name, pred = test_preds, metrics = perf_metrics))
+}
+
+
+predict_test_data_from_data <- function(model_wf, features, cluster_table, metrics_table, 
+                              metric_name, test_region, test_gages){
+  #'
+  #' @description uses the provided model to predict on the test dataset and
+  #' compute performance metrics
+  #'
+  #' @param model_wf model workflow containing a single model that will be used
+  #' to predict on the test_data.
+  #' @param features table of gages (rows) and features (columns). Must include
+  #' COMID and GAGES_ID columns.
+  #' @param cluster_table table of gages (rows) and cluster columns. Must include 
+  #' columns for gage ID, and regions named midhigh and high.
+  #' @param metrics_table table of metrics computed for each gage. Must include
+  #' site_num column.
+  #' @param metric_name character string of the column name in metrics_table to use
+  #' @param test_region vector of 'rain', 'snow', or both. Can use 'all' to 
+  #' use all gages in the metrics_table.
+  #' @param test_gages vector of gage IDs to be used in testing
+  #' 
+  #' @return Returns the predictions and the performance metrics
+  
+  #Get only the metric_name metric
+  metrics_table <- select(metrics_table, site_num, .data[[metric_name]])
+  
+  #determine if the metric should use the high flow region or mid-high flow region
+  if (metric_name %in% c('ma','ml17', 'ml18')){
+    region <- 'midhigh'
+  } else if (!grepl("_q", metric_name)){
+    ##other metrics from HIT
+    region <- 'high'
+  }else{
+    ##get quantile from FDC metric name
+    metric_quantile <- as.numeric(str_split(metric_name, pattern="_q")[[1]][2])
+    region <- ifelse(metric_quantile < 0.75, 'midhigh', 'high')
+  }
+  
+  #Select training region
+  if(test_region == 'all'){
+    metrics_table$region = 'all'
+  }else{
+    #select only gages for high and mid-high model regions
+    if (region == 'high'){
+      metrics_table <- mutate(metrics_table,
+                              region = case_when(site_num %in% cluster_table$ID[cluster_table$high == 4] ~ 'snow',
+                                                 site_num %in% cluster_table$ID[cluster_table$high == 2] ~ 'rain')) %>%
+        drop_na()
+    }else{
+      metrics_table <- mutate(metrics_table,
+                              region = case_when(site_num %in% cluster_table$ID[cluster_table$midhigh == 5] ~ 'snow',
+                                                 site_num %in% cluster_table$ID[cluster_table$midhigh == 3] ~ 'rain'))  %>%
+        drop_na()
+    }
+  }
+  
+  #Select the features for these gages
+  features <- filter(features, GAGES_ID %in% metrics_table$site_num) %>%
+    left_join(metrics_table %>% select(site_num, region), c('GAGES_ID' = 'site_num')) %>%
+    filter(region %in% test_region) %>%
+    filter(GAGES_ID %in% test_gages)
+  
+  #Join dataframe to match the features to the metrics
+  test_data <- left_join(features, metrics_table %>% select(-region), 
+                         by = c('GAGES_ID' = 'site_num')) %>%
+    #select only the features in model_wf
+    select(model_wf$fit$fit$fit$forest$independent.variable.names, contains(metric_name))
+  
+  test_preds <- predict(model_wf, new_data = test_data, type = 'numeric') %>%
+    mutate(obs = test_data[[metric_name]])
+  
+  perf_metrics <- metrics(test_preds, truth = 'obs', estimate = '.pred')
+  
   return(list(metric = metric_name, pred = test_preds, metrics = perf_metrics))
 }

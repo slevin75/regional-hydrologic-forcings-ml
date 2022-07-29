dc5e76ed888632498f3cbbd4fceee67fe253bedf
Sara Levin <slevin@usgs.gov>
2022-07-29 15:08:43 -0500

    changes to plot colors and variograms in response to pr review

--- 6_predict/src/plot_diagnostics.R
+++ 6_predict/src/plot_diagnostics.R
@@ -269,12 +269,11 @@ make_residual_map <- function(df_pred_obs, sites_g2_sf, metric, pred_gage_ids, r
   #' @param df_pred_obs is a data frame with observed (obs) and predicted (.pred) from 
   #' the output of  predict_test_data function. if left null and from_predict is true, 
   #' it will predict it from the model_wf (copied these lines from the plot_pred_obs function)
-  #' @param sites_g2_sf  gagesii spatial ata object with LAT LON and ID
+  #' @param sites_g2_sf  gagesii spatial data object with LAT LON and ID
   #' @param metric  metric that is being predicted
   #' @param pred_gage_ids vector of gage ids that correspond to each row of the df_pred_obs
   #' @param region  region of the model/prediction
   #' @param directory where output figures are saved
-  #' 
 
   if(from_predict){
     #predict from provided workflow and data
@@ -290,30 +289,39 @@ make_residual_map <- function(df_pred_obs, sites_g2_sf, metric, pred_gage_ids, r
     mutate(resid = obs - .pred)
   
   states <- map_data("state")
-  
+  limit <- quantile(df$resid, probs = c( 0.1, 0.9))
   p1<-ggplot(states, aes(x=long, y=lat, group=group)) +
-    geom_polygon(fill="white", colour="gray") +
+    geom_polygon(fill="gray60", colour="gray80") +
     geom_sf(data = df, inherit.aes = FALSE, 
             aes(color = .data[["resid"]]), 
-            size = 0.75)+
-    scale_color_scico(palette = 'batlow')+
+            size = 0.5)+
+    scale_color_scico(palette = 'roma',
+                      midpoint = 0,
+                      limits = limit,
+                      oob = scales::squish)+
     theme(legend.position="bottom",
-          legend.key.size=unit(.5,'cm'))+
+          legend.key.size=unit(.75,'cm'))+
     xlab('Longitude') + 
     ylab('Latitude')+
     ggtitle(paste("metric= ",metric,"    region = ", region ))
   
+  p2 <- plot(variogram(resid ~1, df,
+                       cutoff = 100, 
+                       width = 2,
+                       cressie = TRUE),
+             asp=1)
   
-  p2 <- plot(variogram(resid~1, df))
   
   fname<-paste0(out_dir, "/resid_map_", metric, "_", region, ".png")
   
   save_plot(filename = fname, 
             plot = plot_grid(p1,p2, scale = c(1,.8)),
-            base_width = 8,
+            base_width = 10,
             bg="white")
   
+  
 
+  
 
   return(fname)
 }

75e03d6a4e75842ca71a6709829c69e38fcb41db
Sara Levin <slevin@usgs.gov>
2022-08-19 07:52:49 -0500

    restructured pipeline

---
+++ 2_flow_metrics.R
@@ -0,0 +1,154 @@
+
+source("2_flow_metrics/src/calc_HIT.R")
+source("2_flow_metrics/src/calc_FDC.R")
+
+#####################
+##p2_only  parameters
+
+##statistics to compute within EflowStats
+stats_HIT <- c("calc_magAverage", "calc_magLow", "calc_magHigh", 
+               "calc_frequencyHigh", "calc_durationHigh", "calc_rateChange"
+)
+
+##EflowStats metrics to use
+metrics <- c('ma1', 
+             'ml17', 'ml18', 
+             'mh20', 
+             'fh2', 
+             'dh1', 'dh6', 'dh15', 'dh16', 'dh23',
+             'ra1', 'ra2', 'ra3', 'ra4'
+)
+
+##metrics to normalize by drainage area
+metrics_DA <- c('ma1', 'dh1', 'ra1', 'ra3')
+
+##metric ml17 to normalize by *annual mean/drainage area
+metrics_ml17 <- c('ml17')
+
+##metrics to normalize by *median/drainage area
+metrics_med_DA <- NULL
+
+
+p2_targets_list<- list(
+  ##compute all HIT metrics for screened sites list
+  tar_target(p2_HIT_metrics,
+             calc_HITmetrics(site_num = p1_screened_site_list, 
+                             clean_daily_flow = p1_clean_daily_flow, 
+                             yearType = yearType,
+                             drainArea_tab = p1_drainage_area,
+                             floodThreshold_tab = p1_flood_threshold,
+                             stat_vec = stats_HIT,
+                             save_metrics = metrics,
+                             norm_DA = metrics_DA,
+                             norm_med_DA = metrics_med_DA,
+                             norm_ml17 = metrics_ml17,
+                             out_format = 'pivot'),
+             map(p1_screened_site_list),
+             deployment = 'worker'
+  ),
+  
+  ##compute additional FDC-based metrics for screened sites list
+  tar_target(p2_FDC_metrics,
+             calc_FDCmetrics(site_num = p1_screened_site_list, 
+                             clean_daily_flow = p1_clean_daily_flow, 
+                             yearType = yearType,
+                             drainArea_tab = p1_drainage_area,
+                             NE_probs = NE_quants,
+                             seasonal = FALSE,
+                             year_start = year_start,
+                             out_format = 'pivot'),
+             map(p1_screened_site_list),
+             deployment = 'worker'
+  ),
+  
+  tar_target(p2_FDC_metrics_low,
+             calc_FDCmetrics(site_num = p1_screened_site_list, 
+                             clean_daily_flow = p1_clean_daily_flow, 
+                             yearType = yearType,
+                             drainArea_tab = p1_drainage_area,
+                             NE_probs = NE_quants_low,
+                             seasonal = FALSE,
+                             year_start = year_start,
+                             out_format = 'pivot',
+                             threshold_type = 'low'),
+             map(p1_screened_site_list),
+             deployment = 'worker'
+  ),
+  
+  ##combined metrics tables
+  tar_target(p2_all_metrics,
+             inner_join(p2_FDC_metrics,p2_HIT_metrics)
+  ),
+  #Metrics ml17 and 18 have NAs for some gages, so we will not predict them.
+  #also dropping 0.98, 0.99, and 0.995 metrics
+  tar_target(p2_all_metrics_predict,
+             p2_all_metrics %>% 
+               select(-ml17, -ml18, -contains('0.98'),
+                      -contains('0.99'), -contains('0.995'))
+  ),
+  ##list of all the metrics names - for dynamic branching
+  tar_target(p2_all_metrics_names,
+             colnames(p2_all_metrics)[-1]
+  ),
+  tar_target(p2_all_metrics_names_predict,
+             colnames(p2_all_metrics_predict)[-1]
+  ),
+  tar_target(p2_all_metrics_names_low,
+             colnames(p2_FDC_metrics_low)[
+               -c(1,
+                  #Removing columns with NAs
+                  grep(colnames(p2_FDC_metrics_low), pattern = 'q0.005'),
+                  grep(colnames(p2_FDC_metrics_low), pattern = 'q0.01'))
+             ]
+  ),
+  
+  ##compute seasonal FDC-based metrics using water year seasons
+  tar_target(p2_FDC_metrics_season,
+             calc_FDCmetrics(site_num = p1_screened_site_list_season, 
+                             clean_daily_flow = p1_clean_daily_flow_season, 
+                             yearType = yearType,
+                             drainArea_tab = NULL,
+                             NE_probs = NE_quants,
+                             seasonal = TRUE,
+                             season_months = season_months,
+                             stat_type = 'POR',
+                             year_start = season_year_start,
+                             out_format = 'pivot'),
+             map(p1_screened_site_list_season),
+             deployment = 'worker'
+  ),
+  #Low flow
+  tar_target(p2_FDC_metrics_season_low,
+             calc_FDCmetrics(site_num = p1_screened_site_list_season, 
+                             clean_daily_flow = p1_clean_daily_flow_season, 
+                             yearType = yearType,
+                             drainArea_tab = NULL,
+                             NE_probs = NE_quants_low,
+                             seasonal = TRUE,
+                             season_months = season_months,
+                             stat_type = 'POR',
+                             year_start = season_year_start,
+                             out_format = 'pivot',
+                             threshold_type = 'low'),
+             map(p1_screened_site_list_season),
+             deployment = 'worker'
+  )
+  
+  ##compute seasonal FDC-based metrics using high flow seasons
+  # tar_target(p2_FDC_metrics_season_high,
+  #            calc_FDCmetrics(site_num = p1_screened_site_list_season_high, 
+  #                            clean_daily_flow = p1_clean_daily_flow_season_high, 
+  #                            yearType = yearType,
+  #                            drainArea_tab = NULL,
+  #                            NE_probs = NE_quants,
+  #                            seasonal = TRUE,
+  #                            season_months = season_months_high,
+  #                            stat_type = 'POR',
+  #                            year_start = season_year_start_high,
+  #                            out_format = 'pivot'),
+  #            map(p1_screened_site_list_season_high),
+  #            deployment = 'worker'
+  # )
+  
+  
+)
---
+++ 4_setup_crossval.R
@@ -0,0 +1,108 @@
+source("4_setup_crossval/src/cross_validation_functions.R")
+
+###############
+##p4_params only
+
+###moving window parameters
+window_length <- 20  ##needs to be <= complete_years
+increment <- 1
+min_yrs_in_window<- 15  ##minimum number of years of data required within a window
+min_windows <- 10  ##Must have this many windows available in order to plot 
+###percentage of drainage area overlap between nested basins above which they will be grouped together
+
+nested_threshold<-.5
+##distance to search upstream for nested basins, in km.  note-the nhdplusTools function fails if this 
+##value is 10000 or greater.
+nav_distance_km <- 4500
+
+
+
+p4_targets_list<- list(
+  
+  ########moving window nonstationarity stuff
+  ##table with all the FDC metrics computed on a moving window. The parameter min_yrs_in_window
+  ##screens out any moving windows for which there are too few years to be reliable. Can be an issue 
+  ##when there are large gaps in the data record because a 20 year window might only have a few years of 
+  ##actual data.The yr_ct column indicates how many complete years were in the window just to keep track of it.
+  
+  tar_target(p4_moving_window_metrics,
+             calc_moving_window_metrics(site_num = p1_screened_site_list,
+                                        window_length = window_length,
+                                        increment = increment,
+                                        min_yrs_in_window = min_yrs_in_window,  
+                                        clean_daily_flow = p1_clean_daily_flow,
+                                        yearType = yearType,
+                                        drainArea_tab = p1_drainage_area,
+                                        NE_probs = NE_quants,
+                                        digits = 3, seasonal = FALSE,
+                                        year_start = year_start),
+             map(p1_screened_site_list),
+             deployment = 'main'
+  ),
+  
+  ##screen out any sites that don't have enough moving windows to plot (min_windows)
+  ##using 10 for a default
+  tar_target(p4_screened_plot_sites,
+             screen_plot_sites(moving_window_metrics = p4_moving_window_metrics,
+                               min_windows = min_windows),
+             deployment = 'main'
+  ),
+  
+  tar_target(p4_moving_window_plots_png,
+             make_plots_by_site(site = p4_screened_plot_sites,
+                                moving_window_metrics = p4_moving_window_metrics,
+                                window_length = window_length,
+                                outdir = "1_fetch/out/stationarity_plots"),
+             map(p4_screened_plot_sites),
+             deployment = 'worker',
+             format = "file"
+  ),
+  
+  #CONUS average
+  tar_target(p4_moving_window_summary_plots_png,
+             plot_trend_summary(moving_window_metrics = p4_moving_window_metrics,
+                                screened_plot_sites = p4_screened_plot_sites,
+                                by_cluster = FALSE,
+                                outdir = "1_fetch/out/stationarity_plots"),
+             deployment = 'worker',
+             format = "file"
+  ),
+  
+  #Cluster region average
+  tar_target(p4_moving_window_summary_plots_cluster_png,
+             plot_trend_summary(moving_window_metrics = p4_moving_window_metrics,
+                                screened_plot_sites = p4_screened_plot_sites,
+                                by_cluster = TRUE,
+                                cluster_table = p3_gages_clusters,
+                                cluster_column = p3_cluster_cols,
+                                outdir = "1_fetch/out/stationarity_plots"),
+             map(p3_cluster_cols),
+             deployment = 'worker',
+             format = "file"
+  ),
+  #This will not work because it doesn't know to split by quantile
+  # tar_target(p4_moving_window_summary_plots_cluster_quants_png,
+  #            plot_trend_summary(moving_window_metrics = p4_moving_window_metrics,
+  #                               screened_plot_sites = p4_screened_plot_sites,
+  #                               by_cluster = TRUE,
+  #                               cluster_table = p3_gages_clusters_quants,
+  #                               cluster_column = p3_cluster_cols_quants,
+  #                               outdir = "1_fetch/out/stationarity_plots/by_quantiles"),
+  #            map(p3_cluster_cols_quants),
+  #            deployment = 'worker',
+  #            format = "file"
+  # ),
+  
+  #matrix of nested gages - proportion of overlapping area if column name gage is upstream of the row name gage, 0 otherwise
+  tar_target(p4_nested_gages,
+             get_nested_gages(sites_and_comids = p1_feature_vars_g2,
+                              drainage_areas = p1_drainage_area,
+                              nav_distance_km = nav_distance_km),
+             deployment = 'worker'
+  ),
+  
+  tar_target(p4_nested_groups,
+             add_nested_group_id(p4_nested_gages, p1_drainage_area, nested_threshold))
+  
+  
+)
